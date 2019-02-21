98a83cb5b5ccb5e08d3be861e058130919d9edd9
Rahul Bora <rahulbora@ZCRahulBorasMBP.Dlink>
2019-02-21 22:32:05 +0530

    minor fixes

--- app.js
+++ app.js
@@ -5,15 +5,13 @@ import path from 'path';
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 
-import  indexRouter  from './routes/index';
-import usersRouter  from './routes/users';
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
 
-const  app = express();
+const app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+app.set('view engine' , 'jade')
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,12 +22,13 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+  res.status(404).send('Page Not Found')
 });
 
+
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
